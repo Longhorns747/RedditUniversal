679a0fe177b6a905c00214f0000bfc8dd2b1b0a4
Ethan Shernan <eshernan3@gatech.edu>
2015-05-19 14:33:21 -0400

    WebApp logs user in via Authorization Code grant

--- UniversalRedditLogin/Controllers/HomeController.cs
+++ UniversalRedditLogin/Controllers/HomeController.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using UniversalRedditLogin.Utils;
 
 namespace UniversalRedditLogin.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public void Index()
         {
-            return View();
+            string base_url = @"https://www.reddit.com/api/v1/authorize?client_id=" + RedditInfo.APP_ID + 
+                @"&response_type=code&state=lol&redirect_uri=http://universalredditlogin.azurewebsites.net/home/loggedin" + "&duration=permanent&scope=read,mysubreddits,identity";
+
+            Response.Redirect(base_url);
+        }
+
+        public async Task<string> LoggedIn()
+        {
+            string code = Request.QueryString["code"];
+            string content = "";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.reddit.com/api/v1/access_token");
+                var byteArray = Encoding.UTF8.GetBytes(RedditInfo.APP_ID + ":" + RedditInfo.APP_SECRET);
+                var header = new AuthenticationHeaderValue(
+                           "Basic", Convert.ToBase64String(byteArray));
+                client.DefaultRequestHeaders.Authorization = header;
+
+                var formContent = new FormUrlEncodedContent(new[]
+                {
+                        new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                        new KeyValuePair<string, string>("code", code),
+                        new KeyValuePair<string, string>("redirect_uri", "http://universalredditlogin.azurewebsites.net/home/loggedin")
+                    });
+
+                HttpResponseMessage response = await client.PostAsync(new Uri("https://www.reddit.com/api/v1/access_token"), formContent);
+                content = await response.Content.ReadAsStringAsync();
+            }
+
+            return content;
         }
     }
 }
